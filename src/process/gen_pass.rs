8607d340e8051240f6d5a4c1a1791695d44a891f
krims wang <krimswang@krimss-Mac-mini.local>
2024-04-22 00:26:37 +0800

    feature: support zxcvbn to test password strength

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
 const LOWER: &[u8] = b"abcdefghijkmnopqrstuvwxyz";
@@ -41,6 +42,11 @@ pub fn process_genpass(
     }
     password.shuffle(&mut rng);
 
-    println!("{}", String::from_utf8(password)?);
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    let estimate = zxcvbn(&password, &[])?;
+    eprintln!("Password strength: {}", estimate.score());
+
     Ok(())
 }
