579a35f19ba7bdcee89655b5bdf76b3c2c4e63d2
krims wang <krimswang@krimss-Mac-mini.local>
2024-04-26 01:03:38 +0800

    feature: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -5,6 +5,7 @@ use axum::routing::get;
 use axum::Router;
 use std::path::PathBuf;
 use std::{net::SocketAddr, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -19,9 +20,10 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     info!("Serveing {:?}, on {}", path, port);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
     let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", ServeDir::new(path))
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -34,7 +36,7 @@ async fn file_handler(
     State(state): State<Arc<HttpServeState>>,
     Path(path): Path<String>,
 ) -> (StatusCode, String) {
-    let p = std::path::Path::new(&state.path).join(path);
+    let p = std::path::Path::new(&state.path).join(&path);
     info!("Reading file: {:?}", p);
     if !p.exists() {
         return (
@@ -42,6 +44,11 @@ async fn file_handler(
             format!("File {:?} not found", p.display()),
         );
     }
+    // if p.is_dir() {
+    //     p = std::path::Path::new(&state.path)
+    //         .join(&path)
+    //         .join("index.html");
+    // }
     match tokio::fs::read_to_string(&p).await {
         Ok(content) => {
             info!("Read {} bytes", content.len());
@@ -62,4 +69,19 @@ fn println_http_cli_logo() {
     println!(r"| | ||  | |    | |  |  __/    |  \__| |_/\| |");
     println!(r"\_/ \|  \_/    \_/  \_/       \____/\____/\_/");
     println!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_handle_file() {
+        let path = PathBuf::from(".");
+        let state = HttpServeState { path };
+        let (status, body) =
+            file_handler(State(Arc::new(state)), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(body.trim().starts_with("[package]"));
+    }
 }
