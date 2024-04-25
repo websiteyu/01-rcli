33da4ca1b71e29e772d9d902a78de5ae795d5eee
wangmy <1352546265@qq.com>
2024-04-25 09:24:38 +0800

    remove redundant println

--- src/process/csv_convert.rs
+++ src/process/csv_convert.rs
@@ -24,15 +24,6 @@ pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow:
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
     let headers = reader.headers()?.clone();
-    // let record = reader.deserialize().map(|record| record.unwrap()).collect::<Vec<Player>>();
-    // reader
-    //     .deserialize()
-    //     .map(|record| record.unwrap())
-    //     .collect::<Vec<Player>>()
-    //     .iter()
-    //     .for_each(|record| {
-    //         println!("{:?}", record);
-    //     });
     for result in reader.records() {
         let record = result?;
         let json_value = headers.iter().zip(record.iter()).collect::<Value>();
