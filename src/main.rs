4071f194b1a04917a8c0c7eac754f617ce49626a
krims wang <krimswang@krimss-Mac-mini.local>
2024-04-30 02:15:10 +0800

    use enum_dispatch in fn main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 use clap::Parser;
-use rcli::{CmdExcutor, Opts, SubCommand};
+use rcli::{CmdExcutor, Opts};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
-
-    match opts.cmd {
-        SubCommand::Csv(opts) => opts.execute().await,
-        SubCommand::GenPass(opts) => opts.execute().await,
-        SubCommand::Base64(cmd) => cmd.execute().await,
-        SubCommand::Text(cmd) => cmd.execute().await,
-        SubCommand::Cha1305(cmd) => cmd.execute().await,
-        SubCommand::Http(cmd) => cmd.execute().await,
-    }
-
-    // Ok(())
+    opts.cmd.execute().await
 }
